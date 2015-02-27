a7c344f589b2509398596e1512679ceb03443750
ctrovato <trovato.carmine@gmail.com>
2015-02-27 14:57:24 -0500

    Initial Commit

---
+++ blog/app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Base
+  	has_attached_file :image, styles: { larrge: "600x600>", medium: "300x300>", thumb: "150x150#" }
+  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  	validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
+
+	has_many :comments, dependent: :destroy
+	validates :title, presence: true, length: { minimum: 5 }
+	validates :body,  presence: true
+end
