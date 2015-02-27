6ec00e799a8a01e059a465a736ba6a0f2555b26e
ctrovato <trovato.carmine@gmail.com>
2015-02-27 15:58:15 -0500

    added dropbox.yml - included dropbox access credentials

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -6,4 +6,22 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body,  presence: true
+
+
+	attr_accessible :picture, :verified, :category, :tags, :user_id  
+	#added for paperclip-drop gem   
+	has_attached_file :picture,
+	 :storage => :dropbox,
+	:dropbox_credentials => "#{Rails.root}/config/dropbox_c
+	onfig.yml",
+	 :styles => { :medium => "300x300" , :thumb => "100x100>"},    
+	:dropbox_options => {       
+	:path => proc { |style| "#{style}/#{id}_#{picture.original_filename}"},       :unique_filename => true 
+	  }
+ 
+
+	validates :picture, :attachment_presence => true 
+	validates :tags, :presence =>true     v
+	validates :category, :presence => true    
+	belongs_to :user  
 end
